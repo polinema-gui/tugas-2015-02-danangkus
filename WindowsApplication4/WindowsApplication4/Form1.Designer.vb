431bf7542f0461e7dc7540b87f7b1b82759757df
Danang Kusuma <kdanang20@yahoo.co.id>
2015-10-10 22:50:10 +0700

    Kalkulator
    Danang Kusuma

---
+++ WindowsApplication4/WindowsApplication4/Form1.Designer.vb
@@ -0,0 +1,218 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.nol = New System.Windows.Forms.Button()
+        Me.layar = New System.Windows.Forms.TextBox()
+        Me.satu = New System.Windows.Forms.Button()
+        Me.empat = New System.Windows.Forms.Button()
+        Me.tujuh = New System.Windows.Forms.Button()
+        Me.kurang = New System.Windows.Forms.Button()
+        Me.tambah = New System.Windows.Forms.Button()
+        Me.sembilan = New System.Windows.Forms.Button()
+        Me.enam = New System.Windows.Forms.Button()
+        Me.tiga = New System.Windows.Forms.Button()
+        Me.delapan = New System.Windows.Forms.Button()
+        Me.lima = New System.Windows.Forms.Button()
+        Me.dua = New System.Windows.Forms.Button()
+        Me.samadengan = New System.Windows.Forms.Button()
+        Me.hapus = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'nol
+        '
+        Me.nol.Location = New System.Drawing.Point(12, 207)
+        Me.nol.Name = "nol"
+        Me.nol.Size = New System.Drawing.Size(46, 43)
+        Me.nol.TabIndex = 0
+        Me.nol.Text = "0"
+        Me.nol.UseVisualStyleBackColor = True
+        '
+        'layar
+        '
+        Me.layar.Location = New System.Drawing.Point(12, 13)
+        Me.layar.Multiline = True
+        Me.layar.Name = "layar"
+        Me.layar.Size = New System.Drawing.Size(260, 32)
+        Me.layar.TabIndex = 1
+        '
+        'satu
+        '
+        Me.satu.Location = New System.Drawing.Point(12, 158)
+        Me.satu.Name = "satu"
+        Me.satu.Size = New System.Drawing.Size(46, 43)
+        Me.satu.TabIndex = 2
+        Me.satu.Text = "1"
+        Me.satu.UseVisualStyleBackColor = True
+        '
+        'empat
+        '
+        Me.empat.Location = New System.Drawing.Point(12, 109)
+        Me.empat.Name = "empat"
+        Me.empat.Size = New System.Drawing.Size(46, 43)
+        Me.empat.TabIndex = 3
+        Me.empat.Text = "4"
+        Me.empat.UseVisualStyleBackColor = True
+        '
+        'tujuh
+        '
+        Me.tujuh.Location = New System.Drawing.Point(12, 60)
+        Me.tujuh.Name = "tujuh"
+        Me.tujuh.Size = New System.Drawing.Size(46, 43)
+        Me.tujuh.TabIndex = 4
+        Me.tujuh.Text = "7"
+        Me.tujuh.UseVisualStyleBackColor = True
+        '
+        'kurang
+        '
+        Me.kurang.Location = New System.Drawing.Point(226, 158)
+        Me.kurang.Name = "kurang"
+        Me.kurang.Size = New System.Drawing.Size(46, 43)
+        Me.kurang.TabIndex = 6
+        Me.kurang.Text = "-"
+        Me.kurang.UseVisualStyleBackColor = True
+        '
+        'tambah
+        '
+        Me.tambah.Location = New System.Drawing.Point(226, 207)
+        Me.tambah.Name = "tambah"
+        Me.tambah.Size = New System.Drawing.Size(46, 43)
+        Me.tambah.TabIndex = 5
+        Me.tambah.Text = "+"
+        Me.tambah.UseVisualStyleBackColor = True
+        '
+        'sembilan
+        '
+        Me.sembilan.Location = New System.Drawing.Point(154, 60)
+        Me.sembilan.Name = "sembilan"
+        Me.sembilan.Size = New System.Drawing.Size(46, 43)
+        Me.sembilan.TabIndex = 12
+        Me.sembilan.Text = "9"
+        Me.sembilan.UseVisualStyleBackColor = True
+        '
+        'enam
+        '
+        Me.enam.Location = New System.Drawing.Point(154, 109)
+        Me.enam.Name = "enam"
+        Me.enam.Size = New System.Drawing.Size(46, 43)
+        Me.enam.TabIndex = 11
+        Me.enam.Text = "6"
+        Me.enam.UseVisualStyleBackColor = True
+        '
+        'tiga
+        '
+        Me.tiga.Location = New System.Drawing.Point(154, 158)
+        Me.tiga.Name = "tiga"
+        Me.tiga.Size = New System.Drawing.Size(46, 43)
+        Me.tiga.TabIndex = 10
+        Me.tiga.Text = "3"
+        Me.tiga.UseVisualStyleBackColor = True
+        '
+        'delapan
+        '
+        Me.delapan.Location = New System.Drawing.Point(83, 60)
+        Me.delapan.Name = "delapan"
+        Me.delapan.Size = New System.Drawing.Size(46, 43)
+        Me.delapan.TabIndex = 16
+        Me.delapan.Text = "8"
+        Me.delapan.UseVisualStyleBackColor = True
+        '
+        'lima
+        '
+        Me.lima.Location = New System.Drawing.Point(83, 109)
+        Me.lima.Name = "lima"
+        Me.lima.Size = New System.Drawing.Size(46, 43)
+        Me.lima.TabIndex = 15
+        Me.lima.Text = "5"
+        Me.lima.UseVisualStyleBackColor = True
+        '
+        'dua
+        '
+        Me.dua.Location = New System.Drawing.Point(83, 158)
+        Me.dua.Name = "dua"
+        Me.dua.Size = New System.Drawing.Size(46, 43)
+        Me.dua.TabIndex = 14
+        Me.dua.Text = "2"
+        Me.dua.UseVisualStyleBackColor = True
+        '
+        'samadengan
+        '
+        Me.samadengan.Location = New System.Drawing.Point(83, 207)
+        Me.samadengan.Name = "samadengan"
+        Me.samadengan.Size = New System.Drawing.Size(117, 43)
+        Me.samadengan.TabIndex = 13
+        Me.samadengan.Text = "="
+        Me.samadengan.UseVisualStyleBackColor = True
+        '
+        'hapus
+        '
+        Me.hapus.Location = New System.Drawing.Point(226, 60)
+        Me.hapus.Name = "hapus"
+        Me.hapus.Size = New System.Drawing.Size(46, 92)
+        Me.hapus.TabIndex = 8
+        Me.hapus.Text = "Clear"
+        Me.hapus.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.delapan)
+        Me.Controls.Add(Me.lima)
+        Me.Controls.Add(Me.dua)
+        Me.Controls.Add(Me.samadengan)
+        Me.Controls.Add(Me.sembilan)
+        Me.Controls.Add(Me.enam)
+        Me.Controls.Add(Me.tiga)
+        Me.Controls.Add(Me.hapus)
+        Me.Controls.Add(Me.kurang)
+        Me.Controls.Add(Me.tambah)
+        Me.Controls.Add(Me.tujuh)
+        Me.Controls.Add(Me.empat)
+        Me.Controls.Add(Me.satu)
+        Me.Controls.Add(Me.layar)
+        Me.Controls.Add(Me.nol)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents nol As System.Windows.Forms.Button
+    Friend WithEvents layar As System.Windows.Forms.TextBox
+    Friend WithEvents satu As System.Windows.Forms.Button
+    Friend WithEvents empat As System.Windows.Forms.Button
+    Friend WithEvents tujuh As System.Windows.Forms.Button
+    Friend WithEvents kurang As System.Windows.Forms.Button
+    Friend WithEvents tambah As System.Windows.Forms.Button
+    Friend WithEvents sembilan As System.Windows.Forms.Button
+    Friend WithEvents enam As System.Windows.Forms.Button
+    Friend WithEvents tiga As System.Windows.Forms.Button
+    Friend WithEvents delapan As System.Windows.Forms.Button
+    Friend WithEvents lima As System.Windows.Forms.Button
+    Friend WithEvents dua As System.Windows.Forms.Button
+    Friend WithEvents samadengan As System.Windows.Forms.Button
+    Friend WithEvents hapus As System.Windows.Forms.Button
+
+End Class
---
+++ WindowsApplication4/WindowsApplication4/Form1.vb
@@ -0,0 +1,140 @@
+﻿Public Class Form1
+    Dim a As Integer
+    Dim cek As Byte
+
+    Private Function cekIsi()
+        If (layar.Text.Equals("0") Or Me.cek > 0) Then
+            Return True
+        Else
+            Return False
+        End If
+    End Function
+
+    Private Sub layar_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Button16_Click(sender As Object, e As EventArgs) Handles kurang.Click
+        If (Me.a = 0 Or Me.cek = 2) Then
+            Me.a += Convert.ToInt32(layar.Text)
+        Else
+            Me.a -= Convert.ToInt32(layar.Text)
+        End If
+        layar.Text = Me.a.ToString
+        Me.cek = 1
+    End Sub
+
+    Private Sub empat_Click(sender As Object, e As EventArgs) Handles empat.Click
+        If (cekIsi()) Then
+            layar.Text = "4"
+        Else
+            layar.Text &= "4"
+        End If
+    End Sub
+
+    Private Sub dua_Click(sender As Object, e As EventArgs) Handles dua.Click
+        If (cekIsi()) Then
+            layar.Text = "2"
+        Else
+            layar.Text &= "2"
+        End If
+    End Sub
+
+    Private Sub layar_TextChanged_1(sender As Object, e As EventArgs) Handles layar.TextChanged
+
+    End Sub
+
+    Private Sub tiga_Click(sender As Object, e As EventArgs) Handles tiga.Click
+        If (cekIsi()) Then
+            layar.Text = "3"
+        Else
+            layar.Text &= "3"
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub satu_Click(sender As Object, e As EventArgs) Handles satu.Click
+        If (cekIsi()) Then
+            layar.Text = "1"
+        Else
+            layar.Text &= "1"
+        End If
+    End Sub
+
+    Private Sub lima_Click(sender As Object, e As EventArgs) Handles lima.Click
+        If (cekIsi()) Then
+            layar.Text = "5"
+        Else
+            layar.Text &= "5"
+        End If
+    End Sub
+
+    Private Sub enam_Click(sender As Object, e As EventArgs) Handles enam.Click
+        If (cekIsi()) Then
+            layar.Text = "6"
+        Else
+            layar.Text &= "6"
+        End If
+    End Sub
+
+    Private Sub tujuh_Click(sender As Object, e As EventArgs) Handles tujuh.Click
+        If (cekIsi()) Then
+            layar.Text = "7"
+        Else
+            layar.Text &= "7"
+        End If
+    End Sub
+
+    Private Sub delapan_Click(sender As Object, e As EventArgs) Handles delapan.Click
+        If (cekIsi()) Then
+            layar.Text = "8"
+        Else
+            layar.Text &= "8"
+        End If
+    End Sub
+
+    Private Sub sembilan_Click(sender As Object, e As EventArgs) Handles sembilan.Click
+        If (cekIsi()) Then
+            layar.Text = "9"
+        Else
+            layar.Text &= "9"
+        End If
+    End Sub
+
+    Private Sub nol_Click(sender As Object, e As EventArgs) Handles nol.Click
+        If (cekIsi()) Then
+            layar.Text = "0"
+        Else
+            layar.Text &= "0"
+        End If
+    End Sub
+
+    Private Sub samadengan_Click(sender As Object, e As EventArgs) Handles samadengan.Click
+        Select Case Me.cek
+            Case 1
+                layar.Text = Convert.ToString(Me.a - Convert.ToInt32(layar.Text))
+            Case 2
+                layar.Text = Convert.ToString(Me.a + Convert.ToInt32(layar.Text))
+        End Select
+        Me.a = 0
+    End Sub
+
+    Private Sub tambah_Click(sender As Object, e As EventArgs) Handles tambah.Click
+        If (Me.cek = 1) Then
+            Me.a -= Convert.ToInt32(layar.Text)
+        Else
+            Me.a += Convert.ToInt32(layar.Text)
+        End If
+        layar.Text = Me.a.ToString
+        Me.cek = 2
+    End Sub
+
+    Private Sub clear_Click(sender As Object, e As EventArgs) Handles hapus.Click
+        layar.Text = "0"
+        Me.a = 0
+        Me.cek = 0
+    End Sub
+End Class
